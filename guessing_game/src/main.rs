7a234a3e4b023fb9d413d3546db69cbb863a2e1e
michael <mihai@mandrescu.co>
2017-03-11 15:59:10 +0000

    Using external crate.

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,6 +1,12 @@
+// This is an external dependency. We specified this dependency in the toml file.
+extern crate rand;
+
 // For convenience mostly. We use stdin from io. By doing this here, we don'y have to do this later:
 // str::io::stdin
 use std::io;
+// Rng is "imported" from the rand crate. While we don't use it directly, we need it to use another
+// method. See "traits". ( Sort of like an interface, not really )
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!\nPlease input your guess.");
@@ -11,6 +17,11 @@ fn main() {
     // String has a "associated method" (static method) called "new". That is why we call with ::
     // This is how we tell the compiler guess is a mutable, growable utf-9 encoded array
     let mut guess = String::new();
+
+    // secret_number is immutable. From the rand
+    // We need to "import" Rng at first in order to use the associated method thread_rng and the
+    // method gen_range.
+    let secret_number = rand::thread_rng().gen_range(0, 101);
 
     // io::stdin() from io we call the "associated method" (static methid) stdin to instantiate
     // a instance of the standard input.
@@ -23,4 +34,5 @@ fn main() {
                                         // method.
 
     println!("You guessed: {}", guess);
+    println!("The secret number is: {}", secret_number);
 }
